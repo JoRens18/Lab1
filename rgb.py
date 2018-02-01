6eeb62ff381c220ddf88638935eff63e17d62243
Joey <m185388@usna.edu>
2018-02-01 09:59:59 -0500

    Lab 1 Final

--- rgb.py
+++ rgb.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python
 
 import serial
+import pygame, sys
+from pygame.locals import *
 
-s = serial.Serial("/dev/ttyACM0")
+pygame.init()
+
+DISPLAYSURF = pygame.display.set_mode((400,300))
+pygame.display.set_caption('Change Color!')
+
+s = serial.Serial("/dev/ttyACM1")
 
 while(True):
 	try:
 		l = s.readline()
 		x = l.rstrip().split(",")
-
-		rgb = [ int(val) for val in x]
-	
-	finally:
-		print rgb
 		
+		rgb = [ int(val) for val in x]
+		print(rgb)
+		DISPLAYSURF.fill(rgb) 
+		pygame.display.update()
+	except ValueError:
+		pass
+	
+
 
 
